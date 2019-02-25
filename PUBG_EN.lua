d3a14db6a171e2d884e89888b7118a7ae563a736
Trương Minh Quý <truongminhquy46@gmail.com>
2019-02-25 13:34:22 +0700

    Add files via upload

---
+++ PUBG_EN.lua
@@ -0,0 +1,135 @@
+function MM2512()
+MM0 = gg.multiChoice({
+'🎃 [ʟᴀɴᴅ] ᴀuтo нᴇᴀᴅsнoт [ᴇvᴇʀʏ ԍᴀмᴇ]',
+'🎯 [ʟᴀɴᴅ] wᴇᴀκ ᴀιмʙoт 10м [ᴇvᴇʀʏ ԍᴀмᴇ]',
+'🔫 [ʟᴀɴᴅ] ɴo ʀᴇcoιʟ 50% [cнᴀɴԍᴇ ԍuɴ]',
+'🌱 [ʟᴀɴᴅ] ɴo ԍʀᴀss [ᴇʀᴀɴԍᴇʟ] [ᴇvᴇʀʏ ԍᴀмᴇ]',
+'⚪ [ʟoʙʙʏ] wнιтᴇ coʟoʀ [ʀuɴ oɴᴇ тιмᴇ]',
+'⚫ [ʟoʙʙʏ] ʙʟᴀcκ coʟoʀ [ʀuɴ oɴᴇ тιмᴇ]',
+'🚹 [ʟoʙʙʏ] ᴀɴтᴇɴᴀ [ʀuɴ oɴᴇ тιмᴇ]',
+'⛔   ᴇxιт   ⛔'
+}, nil, '🇨🇳 VIP Hack PUBG By MM2512 🇨🇳')
+if MM0 == nil then
+  else
+if MM0[1] == true then AHS() end
+if MM0[2] == true then WAB() end
+if MM0[3] == true then NRC() end
+if MM0[4] == true then NGR() end
+if MM0[5] == true then ACL() end
+if MM0[6] == true then BCL() end
+if MM0[7] == true then ATN() end
+if MM0[8] == true then Exit() end
+end
+PUBGEN = -1
+end
+
+function ACL()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('573.70306396484;0.05499718338;1::50', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll('999', gg.TYPE_FLOAT)
+gg.toast('✔ wнιтᴇ coʟoʀ ✔')
+gg.clearResults()
+end
+function BCL()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('573.70306396484;0.05499718338;1::50', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll('-999', gg.TYPE_FLOAT)
+gg.toast('✔ ʙʟᴀcκ coʟoʀ ✔')
+gg.clearResults()
+end
+function AHS()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('-88.66608428955;26:512', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('26', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll('-460', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('-88.73961639404;28:512', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('28', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll('-560', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('9.201618;30.5;25', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('25;30.5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll('250', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('✔ ᴀuтo нᴇᴀᴅsнoт ✔')
+gg.clearResults()
+end
+
+function WAB()
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber('360.0;0.0001;1,478,828,288.0::', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResultsCount()
+gg.searchNumber('0.0001', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll('1998', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('✔ wᴇᴀκ ᴀιмʙoт 10м ✔')
+gg.clearResults()
+end
+function NRC()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('176293393;8F;9.5F;15F::', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('176293393', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll('0', gg.TYPE_DWORD)
+gg.toast('✔ ɴo ʀᴇcoιʟ 50% ✔')
+gg.clearResults()
+end
+function NGR()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('8.0F;1.20000004768F;0.80000001192F;1.5F;0.80000001192F;1.5F:512', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('8', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll('0', gg.TYPE_FLOAT)
+gg.toast('✔ ɴo ԍʀᴀss [ᴇʀᴀɴԍᴇʟ] ✔')
+gg.clearResults()
+end
+function ATN()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('88.50576019287F;87.27782440186F;1F::50', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(7)
+gg.editAll('1.96875', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('1.96875F;1.96875F;-100.91194152832;1F::50', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll('976', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber('0.98900693655~0.98900723457;0.14786802232~0.14786840975;1.1920926e-7::9', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResultsCount()
+gg.searchNumber('0.98900693655~0.98900723457', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll('16000', gg.TYPE_FLOAT)
+gg.toast('✔ ᴀɴтᴇɴᴀ ✔')
+gg.clearResults()
+end
+
+function Exit()
+  gg.skipRestoreState()
+  os.exit()
+  gg.setVisible(true)
+end
+while true do
+if gg.isVisible(true) then
+    PUBGEN = 1
+    gg.setVisible(false)
+  end
+gg.clearResults()
+if PUBGEN == 1 then
+    MM2512()
+  end
+end
